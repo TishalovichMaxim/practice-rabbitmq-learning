eb284133945379f1d59984cb1233864d086ee79e
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-20 15:38:31 +0300

    Add rabbitmq consumer

---
+++ src/main/java/by/tishalovich/rabbitmq/consumer/RabbitmqConsumer.java
@@ -0,0 +1,19 @@
+package by.tishalovich.rabbitmq.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitmqConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitmqConsumer.class);
+
+    @RabbitListener(queues = {"${spring.rabbitmq.queue.name}"})
+    public void consume(Message<String> message) {
+        LOGGER.info(String.format("Consumed message: %s", message.getPayload()));
+    }
+
+}
